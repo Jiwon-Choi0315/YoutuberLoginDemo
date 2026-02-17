7499a9966c39e302dea3cd2a30b07c02fc67edf8
Jiwon-Choi0315 <garotii0315@gmail.com>
2026-02-17 12:18:51 +0900

    DB와 연결 channels, users DB 사용하도록 코드 수정 코드 리팩토링 조금

--- routes/channels.js
+++ routes/channels.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 router.use(express.json());
+const conn = require("../mariadb");
 
 let db = new Map();
 var id = 1;
@@ -9,42 +10,65 @@ router
     .route("/")
     .post((req, res) => {
         const {channelTitle} = req.body;
-        let channel = req.body;
-        db.set(id++, channel);
+        let {name, uid} = req.body;
 
-        if (channelTitle) { 
-            res.status(201).json({
-                message: `${channelTitle} 채널을 생성하였습니다.`
-            })
-        } else {
-            res.status(400).json({
-                message: "채널 이름이 입력되지 않았습니다."
-            })
+        conn.query(
+        `INSERT INTO channels (name, uid)
+        VALUES (?, ?)`, [name, uid],
+        function (err, results) {
+            if (err) {
+                // 터미널에서 오류 확인할 수 있음
+                console.error("DB 오류 발생: ", err);
+                res.status(500).json({ message: "DB 저장 중 에러가 발생했습니다." });
+            } else {
+                res.status(201).json({
+                    message: `${name}채널 생성을 축하드립니다..`,
+                    result: results
+                });
+            }
         }
+    );
     }) // 채널 개별 생성
     .get((req, res) => {
-        var {userId} = req.body;
-        var channels = [];
+        var {uid} = req.body;
 
-        // userId가 body에 존재하는 경우
-        if (db.size && userId) {
-            db.forEach((value, key) => {
-                if (value.userId == userId) {
-                    channels.push(value);
+        if(uid) {
+            conn.query(
+                `SELECT * FROM channels WHERE uid = ?`, uid,
+                function (err, results) {
+                    if (results.length) {
+                        res.status(200).json(results);
+                    } else {
+                        res.status(404).json({ message: "해당 사용자를 찾지 못했습니다." });
+                    }
                 }
-            })
+            )
+        } else {
+            res.status(400).end();
+        }
 
-            // 해당 사용자가 소유한 채널이 존재하는 경우
-            if (channels.length) {
-                res.json(channels);
-            }
-            else {
-                channelNotFound();
-            }
-        }
-        else {
-            channelNotFound();
-        }
+        // var {userId} = req.body;
+        // var channels = [];
+
+        // // userId가 body에 존재하는 경우
+        // if (db.size && userId) {
+        //     db.forEach((value, key) => {
+        //         if (value.userId == userId) {
+        //             channels.push(value);
+        //         }
+        //     })
+
+        //     // 해당 사용자가 소유한 채널이 존재하는 경우
+        //     if (channels.length) {
+        //         res.json(channels);
+        //     }
+        //     else {
+        //         channelNotFound(res);
+        //     }
+        // }
+        // else {
+        //     channelNotFound(res);
+        // }
     }) // 채널 전체 조회
 
 router
@@ -88,20 +112,21 @@ router
         let {id} = req.params;
         id = parseInt(id);
 
-        var channel = db.get(id);
-
-        if (channel) {
-            res.status(200).json(channel);
-        }
-        else {
-            channelNotFound();
-        }
+        conn.query(
+            `SELECT * FROM channels WHERE id = ?`, id,
+            function (err, results) {
+                if (results.length)
+                    res.status(200).json(results);
+                else
+                    channelNotFound(res);
+            }
+        );
     }) // 채널 개별 조회
 
-    function channelNotFound() {
-        res.status(404).json({
-                message: "해당하는 채널이 없습니다."
-            })
-    }
+function channelNotFound(res) {
+    res.status(404).json({
+            message: "해당하는 채널이 없습니다."
+        })
+}
 
-    module.exports = router;
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,101 +1,109 @@
 const express = require('express');
 const router = express.Router();
 router.use(express.json());
+const conn = require('../mariadb');
 
 console.log("App is running on port 1234.");
 
 
 // 로그인
 router.post("/login", (req, res) => {
-    let {uid} = req.body;
-    let {pwd} = req.body;
-    let user = db.get(uid);
+    const {email, password} = req.body;
 
-    if (user) {
-        if (user.pwd == pwd){
-            res.json({message: `${db.get(uid).name}님의 로그인을 환영합니다.`})
+    conn.query(
+        `SELECT * FROM users WHERE email = ?`, email,
+        function (err, results) {
+            let user = results[0];
+
+            if (user && password == user.password) {
+                    res.status(200).json({message: `${user.name}님 환영합니다.`});
+            } else {
+                res.status(404).json({ message: "입력한 정보가 틀렸습니다." });
+            }
         }
-        else {
-            res.status(404).json({message: "비밀번호가 틀렸습니다."});
-        }
-    }
-    else {
-        res.status(404).json({message: "해당 사용자를 찾지 못했습니다."});
-    }
+    );
 })
 
 // 회원가입
 router.post("/signup", (req, res) => {
-    let {uid} = req.body;
-    let {name} = req.body;
-    db.set(uid, req.body);
+    const { email, name, password, contact } = req.body;
 
-    if (uid && name && req.body.pwd) {
-        res.status(201).json({
-        message: `${name}님의 회원가입을 환영합니다.`
-        })
-    }
-    else {
-        res.status(400).json({message: "입력된 정보가 잘못되었습니다."});
-    }
-
+    conn.query(
+        `INSERT INTO users (email, name, password, contact)
+        VALUES (?, ?, ?, ?)`, [email, name, password, contact],
+        function (err, results) {
+            if (err) {
+                // 터미널에서 오류 확인할 수 있음
+                console.error("DB 오류 발생: ", err);
+                res.status(500).json({ message: "DB 저장 중 에러가 발생했습니다." });
+            } else {
+                res.status(201).json({
+                    message: `${name}님의 회원가입을 환영합니다.`,
+                    result: results
+                });
+            }
+        }
+    );
 })
 
 router
     .route("/users")
     // 회원 정보 개별 조회
     .get((req, res) => {
-        let {uid} = req.body;
+        let { email } = req.body;
 
-        if (db.get(uid)) {
-            let name = db.get(uid).name;
-
-            res.status(200).json({
-                message: `uid: ${uid}, name: ${name}`
-            })
-        }
-        else {
-            res.status(404).json({message: "해당 사용자를 찾지 못했습니다."});
-        }
+        conn.query(
+            `SELECT * FROM users WHERE email = ?`, email,
+            function (err, results) {
+                if (results.length)
+                    res.status(200).json(results);
+                else
+                    res.status(404).json({ message: "해당 사용자를 찾지 못했습니다." });
+            }
+        );
     })
 
     // 회원 개별 탈퇴
     .delete((req, res) => {
-        let {uid} = req.params;
+        const { email } = req.body;
+        var name = "";
 
-        if (db.get(uid)) {
-            let name = db.get(uid).name;
-            
-            db.delete(uid);
+        // 메세지에 출력할 이름 미리 찾아놓기
+        conn.query(
+            `SELECT name FROM users WHERE email =  ?`, email,
+            function (err, results, fields) {
+                // 삭제할 대상의 이름을 찾으면
+                if (results.length) {
+                    if (err) {
+                        console.error("DB 오류 발생: ", err);
+                        res.status(500).json({ message: "유저를 검색하는 과정에서 오류가 발생했습니다." });
+                    } else {
+                        name = results[0].name;
+                    }
+                }
+                // 삭제할 대상이 없는 경우 
+                else {
+                    console.error("삭제할 대상이 존재하지 않음")
+                    res.status(404).json({message: "삭제할 대상을 찾지 못했습니다."});
+                }
 
-            res.status(200).json({
-                message: `${name}님 나중에 다시 보기를 바라겠습니다.`
-            })
-        }
-        else {
-            res.status(404).json({message: "해당 사용자를 찾지 못했습니다."});
-        }
+
+                // 유저 삭제하기
+                conn.query(
+                    `DELETE FROM users WHERE email =  ?`, email,
+                    function (err, results) {
+                        if (err) {
+                            console.error("DB 오류 발생: ", err);
+                            res.status(500).json({ message: "유저를 삭제하는 과정에서 오류가 발생했습니다." });
+                        } else {
+                            res.status(200).json({ message: `${name}님 다시 만나기를 기대하겠습니다.`,
+                                                result: results})
+                        }
+                    }
+                )
+            }
+        )
     })
 
-router.delete("/users/:uid", (req, res) => {
-    let {uid} = req.params;
-
-    if (db.get(uid)) {
-        let name = db.get(uid).name;
-        
-        db.delete(uid);
-
-        res.status(200).json({
-            message: `${name}님 나중에 다시 보기를 바라겠습니다.`
-        })
-    }
-    else {
-        res.status(404).json({message: "해당 사용자를 찾지 못했습니다."});
-    }
-})
-
-
-let db = new Map;
-var id = 1;
 
 module.exports = router;
